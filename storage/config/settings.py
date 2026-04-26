7211671c5af9caaa43cfc302dc4a30dad2d608dd
NikolaySolop <nikolay.solop@yandex.ru>
2026-04-26 23:41:06 +0400

    infra v0.2

---
+++ storage/config/settings.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    postgres_db: str
+    postgres_user: str
+    postgres_password: str
+    postgres_host: str = "postgres"
+    postgres_port: int = 5432
+
+    @property
+    def database_url(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.postgres_user}:"
+            f"{self.postgres_password}@{self.postgres_host}:"
+            f"{self.postgres_port}/{self.postgres_db}"
+        )
+
+
+settings = Settings()
